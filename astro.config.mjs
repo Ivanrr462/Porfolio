13d42451be5b345d0e70f2c8450e253ac6a7db27
Ivan Rios <ivan.rios1712@gmail.com>
2026-06-29 13:20:59 +0200

    mejoras en optimización y carga de fotos

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,26 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+    build: {
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          drop_console: true,
+        },
+      },
+    },
+    ssr: {
+      noExternal: [],
+    },
+  },
+  compressHTML: true,
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp',
+    },
+    remotePatterns: [
+      { protocol: 'https' }
+    ],
+  },
 });
